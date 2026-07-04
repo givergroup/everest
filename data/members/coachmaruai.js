e56a6a8d7c7f95bb1f3db5fe0986cd98c79ce465
givergroup <givergroupschool@gmail.com>
2026-07-05 06:41:50 +0700

    Create coachmaruai.js

---
+++ data/members/coachmaruai.js
@@ -0,0 +1,9 @@
+ }
+  "coachmaruai": {
+    "name_th": "โค้ชมารวย",
+    "name_en": "Coach Maruai",
+    "phone": "0955636564",
+    "line_url": "https://line.me/ti/p/klic4L83Cz",
+    "register_link": "https://everest-media.co/register?ref=E000817",
+    "image_url": "/en/images/coachmaruai.jpg"
+  }
