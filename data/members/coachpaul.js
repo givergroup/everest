92f200ea0ce70b2ab2c3072b1797f1ba28d30046
givergroup <givergroupschool@gmail.com>
2026-07-05 06:24:23 +0700

    Create coachpaul.js

---
+++ data/members/coachpaul.js
@@ -0,0 +1,9 @@
+{
+  "coachpaul": {
+    "name_th": "โค้ชพล",
+    "name_en": "Coach Paul",
+    "phone": "+66952539634",
+    "line_url": "https://lin.ee/ScFHI5r",
+    "register_link": "https://everest-media.co/register?ref=E000694",
+    "image_url": "/en/images/coachpaul.png"
+  }
