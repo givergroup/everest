18dcd44f18a571ab7a997d937653a6fe4d89606f
givergroup <givergroupschool@gmail.com>
2026-07-05 06:34:05 +0700

    Create coachpal.js

---
+++ data/members/coachpal.js
@@ -0,0 +1,9 @@
+{
+    "coachpal": {
+    "name_th": "โค้ชปาล",
+    "name_en": "Coach Pal",
+    "phone": "093-9791791",
+    "line_url": "https://line.me/ti/p/E7tuyxzg4Q",
+    "register_link": "https://everest-media.co/register?ref=E005256",
+    "image_url": "/en/images/coachpal.jpg"
+  }
