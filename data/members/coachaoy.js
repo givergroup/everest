e3df3887005c030384ae84387e20b880bde53de1
givergroup <givergroupschool@gmail.com>
2026-07-05 07:14:59 +0700

    Update coachaoy.js

--- data/members/coachaoy.js
+++ data/members/coachaoy.js
@@ -1,9 +1,8 @@
 {
-    "coachaoy": {
-    "name_th": "โค้ช อ่อย",
-    "name_en": "Coach Aoy",
-    "phone": "098-167-0132",
-    "line_url": "https://line.me/ti/p/_UnmGdysG3",
-    "register_link": "https://everest-media.co/register?ref=E002735",
-    "image_url": "/en/images/coachaoy.jpg"
-  }
+  "name_th": "โค้ช อ้อย",
+  "name_en": "Coach Aoy",
+  "phone": "098-167-0132",
+  "line_url": "https://line.me/ti/p/_UnmGdysG3",
+  "register_link": "https://everest-media.co/register?ref=E002735",
+  "image_url": "/en/images/coachaoy.jpg"
+}
